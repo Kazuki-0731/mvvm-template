b1598047c1e04cbfe04e516071dcf50822aa9baa
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-11 19:56:15 +0900

    create calc activity

---
+++ app/src/main/java/com/example/recruitment_examination/view/CalcActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recruitment_examination.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recruitment_examination.R
+
+class CalcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
