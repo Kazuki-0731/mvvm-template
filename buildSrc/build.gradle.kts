3fd509b65a23d13373a5e88b59df24bea7d7cd1e
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-11 22:35:46 +0900

    create buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:4.1.2")
+}
+
---
+++ buildSrc/src/main/java/com/example/recruitment_examination/build/Dep.kt
@@ -0,0 +1,11 @@
+package com.example.recruitment_examination.build
+
+object Dep {
+    object Dagger {
+        const val core = "com.google.dagger:dagger:2.27"
+        const val android = "com.google.dagger:dagger-android:2.27"
+        const val androidSupport = "com.google.dagger:dagger-android-support:2.27"
+        const val compiler = "com.google.dagger:dagger-compiler:2.40"
+        const val androidProcessor = "com.google.dagger:dagger-android-processor:2.27"
+    }
+}
