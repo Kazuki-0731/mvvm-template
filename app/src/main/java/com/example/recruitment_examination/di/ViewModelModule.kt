98e4f8c29c69208c75838a48cd168889679c5d2f
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-12 03:44:56 +0900

    viewModelFactory update

--- app/src/main/java/com/example/recruitment_examination/di/ViewModelModule.kt
+++ app/src/main/java/com/example/recruitment_examination/di/ViewModelModule.kt
@@ -1,13 +1,18 @@
 package com.example.recruitment_examination.di
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.recruitment_examination.viewmodel.CalcViewModel
+import com.example.recruitment_examination.viewmodel.ViewModelFactory
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
     @Binds
     @IntoMap
     @ViewModelKey(CalcViewModel::class)
--- app/src/main/java/com/example/recruitment_examination/viewmodel/CalcViewModel.kt
+++ app/src/main/java/com/example/recruitment_examination/viewmodel/CalcViewModel.kt
@@ -1,9 +1,8 @@
 package com.example.recruitment_examination.viewmodel
 
 import androidx.lifecycle.ViewModel
-import javax.inject.Inject
 
-class CalcViewModel @Inject constructor() : ViewModel() {
+class CalcViewModel : ViewModel() {
     // データ保持領域
 
     // onClick処理
