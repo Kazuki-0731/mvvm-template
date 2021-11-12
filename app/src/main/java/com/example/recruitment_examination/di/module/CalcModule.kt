b0e8120b9e3ffb8eea918df6e2de5141be952ef6
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-12 21:55:32 +0900

    delete CalcModule.kt, CalcBuilder.kt. CalcFragment.kt fix

--- app/src/main/java/com/example/recruitment_examination/di/module/CalcModule.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recruitment_examination.di.module
-
-/**
- * ActivityごとのModule
- */
-class CalcModule {
-}
--- app/src/main/java/com/example/recruitment_examination/view/CalcFragment.kt
+++ app/src/main/java/com/example/recruitment_examination/view/CalcFragment.kt
@@ -1,29 +1,26 @@
 package com.example.recruitment_examination.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.recruitment_examination.R
 import com.example.recruitment_examination.databinding.FragmentCalcBinding
 import com.example.recruitment_examination.di.Injectable
-import timber.log.Timber
+import com.example.recruitment_examination.viewmodel.CalcViewModel
+import javax.inject.Inject
 
 class CalcFragment : Fragment(), Injectable {
-//    @Inject
-//    lateinit var viewModelFactory: ViewModelProvider.Factory
-//    private val viewModel :CalcViewModel by lazy {
-//        ViewModelProvider.NewInstanceFactory().create(CalcViewModel::class.java)
-//    }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel :CalcViewModel by lazy {
+        ViewModelProvider.NewInstanceFactory().create(CalcViewModel::class.java)
+    }
 
     private lateinit var binding: FragmentCalcBinding
-//    private lateinit var viewModel: CalcViewModel
-//    private val viewModel: CalcViewModel by viewModels {
-//        viewModelFactory
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,25 +34,9 @@ class CalcFragment : Fragment(), Injectable {
             false
         )
 
-//        viewModel = ViewModelProvider(
-//            requireActivity(),
-//            viewModelFactory
-//        ).get(CalcViewModel::class.java)
-//        viewModel.navigator = this
-//        binding.viewModel = viewModel
-
-//        viewModel.test()
-
-        Log.d("debug", "CalcFragment onCreateView")
-        Timber.d("CalcFragment onCreateView")
+        viewModel.test()
 
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.d("debug", "CalcFragment onViewCreated")
-        Timber.d("CalcFragment onViewCreated")
     }
 
     companion object {
